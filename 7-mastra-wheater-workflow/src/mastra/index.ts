354c4b1d553fabbdb148c0229eff489160aed0cc
Hamza mounir <hamza.pixelle@gmail.com>
2025-04-13 05:33:17 +0200

    add mastra workflow a

---
+++ 7-mastra-wheater-workflow/src/mastra/index.ts
@@ -0,0 +1,12 @@
+import { Mastra } from "@mastra/core/mastra";
+import { createLogger } from "@mastra/core/logger";
+import { weatherWorkflow } from "./workflows";
+
+export const mastra = new Mastra({
+  workflows: { weatherWorkflow },
+
+  logger: createLogger({
+    name: "Mastra",
+    level: "info",
+  }),
+});
